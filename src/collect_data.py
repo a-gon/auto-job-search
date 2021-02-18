eea3bfd53e585045cc8726251bc6f749a9e71bf0
Anna Goncharova <annab.goncharova@gmail.com>
2021-02-17 20:07:55 -0800

    Added Job_Posting class support, separated driver, extract and store code

--- src/collect_data.py
+++ src/collect_data.py
@@ -5,68 +5,34 @@ import job_filter as f
 import hash as h
 from os.path import isfile
 import data_store as ds
+from job_posting_class import Job_Posting
 
-def collect_data(driver, job_container):
+
+def store_data(job_list):
     '''
-    collect_data function: receives the driver object and a container with job postings,
-    visits each job posting and collects the required data
+    job_posting -> Job_Posting object
+    checks that the job posting has already been seen before
     avoids saving duplicate job postings into the database by hashing the job description 
     creates the database if it does not exist yet
     '''
-    if len(job_container) == 0:
-        raise ValueError('Job container is empty. To proceed, it must contain at least one item.')
-    if not driver:
-        raise RuntimeError('Driver object is incorrect.')
-
-    print(f'Getting information about {len(job_container)} jobs.')
-
+    if not job_list:
+        raise ValueError('Job list is empty. To proceed, it must contain at least one item.')
+    
     if not isfile('visited_jobs.db'):
         ds.createdb()
 
-    job_postings = []
-   
-    for job in job_container:
-        job_id = job.find('a', href=True)['href']
-        job_id = re.findall(r'(?!-)([0-9]*)(?=\?)',job_id)[0]
-        job_postings.append(job_id)
-
-    title, date_posted, link, company, location, job_id, level = None, None, None, None, None, None, None
     accepted, not_accepted = 0, 0
 
-    for x in range(1,len(job_postings)+1):
-        try:
-            job_xpath = '/html/body/main/div/section/ul/li[{}]/img'.format(x)
-            driver.find_element_by_xpath(job_xpath).click()
-            sleep(3)
-
-            title = driver.find_element_by_class_name('topcard__title').text
-
-            link = driver.find_element_by_xpath('/html/body/main/section/div[2]/section/div[1]/div[1]/a').get_attribute('href')
-
-            company = driver.find_element_by_class_name('topcard__flavor').text
-
-            location = driver.find_element_by_class_name('topcard__flavor--bullet').text
-
-            job_id = driver.find_elements_by_id('jobId')
-            
-            level = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/ul/li[1]/span').text
-
-            job_desc = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/div/section/div').text
-
-            date_posted = date.today().strftime("%m/%d/%y")
-        except RuntimeError as e:
-            print('Runtime error while processing a job posting')
-            print(e.args)
-            print(e.with_traceback)
-
-        job_hash = h.get_hash(job_desc)
+    for job_posting in job_list:
+        job_posting.hash = h.get_hash(job_posting.description)
         # print(job_hash)
-        if h.is_in_accepted(job_hash) or h.is_in_notaccepted(job_hash):
-            continue
-        elif f.accepted_title(title) and f.accepted_level(level) and f.accepted_description(job_desc):
-            ds.insert_accepted_job((job_hash, date_posted, title, company, location, level, job_desc, link))
+        if h.is_in_accepted(job_posting.hash) or h.is_in_notaccepted(job_posting.hash):
+            return
+        elif f.accepted_title(job_posting.title) and f.accepted_level(job_posting.level) and f.accepted_description(job_posting.description):
+            ds.insert_accepted_job((job_posting.hash, job_posting.date_posted, job_posting.title, job_posting.company, job_posting.location, job_posting.level, job_posting.description, job_posting.link))
             accepted += 1
         else:
-            ds.insert_notaccepted_job((job_hash, date_posted))     
+            ds.insert_notaccepted_job((job_posting.hash, job_posting.date_posted))     
             not_accepted += 1
+
     print(f'Jobs accepted: {accepted}\nJobs not accepted: {not_accepted}') 
--- src/linkedinbot.py
+++ src/linkedinbot.py
@@ -4,55 +4,115 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from time import time
+from time import sleep
+from datetime import date
+import re
+
+from job_posting_class import Job_Posting
 start_time = time()
-import collect_data as cd
+from collect_data import store_data
 
-# url = "https://www.linkedin.com/jobs/search/?f_E=2%2C1&f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD"
-url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD&redirect=false&position=1&pageNum=0&f_TP=1%2C2'
-no_of_jobs = 25
-# getVars = {'f_E' : '2', 'f_TPR' : 'r86400', 'geoId': '102095887', 'keywords' : job_title, 'location' : location, 'sortBy': 'DD'}
-# url = ('https://www.linkedin.com/jobs/search/?' + urllib.parse.urlencode(getVars))
-# print("url: ", url)
-options = Options()
-options.add_argument('--headless')    # without opening Chrome window
+def launch_driver(driver, url):
+    # url = "https://www.linkedin.com/jobs/search/?f_E=2%2C1&f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD"
+    # url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD&redirect=false&position=1&pageNum=0&f_TP=1%2C2'
+    # getVars = {'f_E' : '2', 'f_TPR' : 'r86400', 'geoId': '102095887', 'keywords' : job_title, 'location' : location, 'sortBy': 'DD'}
+    # url = ('https://www.linkedin.com/jobs/search/?' + urllib.parse.urlencode(getVars))
+    # print("url: ", url)
+    # options = Options()
+    # options.add_argument('--headless')    # without opening Chrome window
 
-driver = webdriver.Chrome('/Users/anna/projects-sept2020/linkedin-bot/chromedriver', options=options)
-driver.get(url)
+    # driver = webdriver.Chrome('src/chromedriver', options=options)
+    driver.get(url)
+    if not driver:
+        raise RuntimeError('Driver object is incorrect.')
+
+    class infinite_scroll(object):
+      def __init__(self, last):
+        self.last = last
+      def __call__(self, driver):
+        new = driver.execute_script('return document.body.scrollHeight')  
+        if new > self.last:
+            return new
+        else:
+            return False
+    last_height = driver.execute_script('return document.body.scrollHeight')
+    flag = 1
+    while flag == 1:
+      driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+      try:
+        wait = WebDriverWait(driver, 10)
+        new_height = wait.until(infinite_scroll(last_height))
+        last_height = new_height
+      except:
+          print("End of page reached")
+          flag = 0
+
+    # pageSource = driver.page_source
+    lxml_soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    job_container = lxml_soup.find('ul', class_ = 'jobs-search__results-list')
+    print('Job postings found:', len(job_container))
+    return job_container
 
 
-class infinite_scroll(object):
-   
-  def __init__(self, last):
+def extract_data(driver, job_container):
+    ''' 
+    driver -> web driver, Chrome
+    job_container -> a web container with all job postings
+    Extracts all jobs postings and theri attributes from the job container
+    Returns a list of Job_Posting objects 
+    '''
+    if len(job_container) == 0:
+        raise ValueError('Job container is empty. To proceed, it must contain at least one item.')
     
-    self.last = last
+    job_postings = []
 
-  def __call__(self, driver):
-    new = driver.execute_script('return document.body.scrollHeight')  
-    if new > self.last:
-        return new
-    else:
-        return False
 
-last_height = driver.execute_script('return document.body.scrollHeight')
-flag=1
-while flag==1:
+    for i in range(1, len(job_container)+1):
+        # print(i)
+        # job_id = i.find('a', href=True)['href']
+        # job_id = re.findall(r'(?!-)([0-9]*)(?=\?)',job_id)[0]
+        # # job_postings.append(job_id)
+        # print(f'job_id = {job_id}')
 
-  driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        try:
+            job_xpath = f'/html/body/main/div/section/ul/li[{i}]/img'
+            driver.find_element_by_xpath(job_xpath).click()
+            sleep(3)
+            job = Job_Posting
 
-  try:
-   wait = WebDriverWait(driver, 10)
+            job.title = driver.find_element_by_class_name('topcard__title').text
 
-   new_height = wait.until(infinite_scroll( last_height))
-   last_height = new_height
+            job.link = driver.find_element_by_xpath('/html/body/main/section/div[2]/section/div[1]/div[1]/a').get_attribute('href')
 
-  except:
-      print("End of page reached")
-      flag = 0
+            job.company = driver.find_element_by_class_name('topcard__flavor').text
 
-pageSource = driver.page_source
-lxml_soup = BeautifulSoup(pageSource, 'lxml')
+            job.location = driver.find_element_by_class_name('topcard__flavor--bullet').text
 
-job_container = lxml_soup.find('ul', class_ = 'jobs-search__results-list')
-print('Job postings found:', len(job_container))
+            job.level = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/ul/li[1]/span').text
 
-cd.collect_data(driver, job_container)
+            job.description = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/div/section/div').text
+
+            job.date_posted = date.today().strftime("%m/%d/%y")
+
+            job_postings.append(job)
+
+        except Exception as e:
+            print('Runtime error while processing a job posting')
+            print(e.args)
+            print(e.with_traceback)
+
+    return job_postings
+
+
+if __name__ == "__main__":
+    url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD&redirect=false&position=1&pageNum=0&f_TP=1%2C2'
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome('src/chromedriver', options=options)
+
+    job_container = launch_driver(driver, url)
+    job_postings = extract_data(driver, job_container)
+    
+    store_data(job_postings)
+
