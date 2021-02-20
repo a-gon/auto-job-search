2869972b652a81bf969d549fd7f6f202d4034b9c
Anna Goncharova <annab.goncharova@gmail.com>
2021-02-20 12:03:19 -0800

    Deleted unused code, updated requirements

--- src/hash.py
+++ src/hash.py
@@ -1,34 +1,24 @@
 import hashlib
-# import shelve
-from os import path
 import data_store as ds
 
 def get_hash(job_description):
+    """
+    get_hash: takes in a text and returns a hash key
+    """
     job_description = job_description.encode('utf-8')
-    # print(job_description)
     hash_object = hashlib.md5(job_description.lower())
     return hash_object.hexdigest()
 
-# def is_new(hash):
-#     if not path.exists("jobs"):
-#         return True
-#     with shelve.open('jobs') as jobs:
-#         if hash in jobs['applied']:
-#             print('Exists in db')
-#             return False
-#     return True
-
-# def add_to_db(hash):
-#     with shelve.open('jobs') as jobs:
-#         if not jobs:
-#             jobs['applied'] = {hash}
-#         else:
-#             jobs['applied'].add(hash)
-#     print('Added to database')
-
 def is_in_accepted(hash):
+    """
+    is_in_accepted: takes in a hash (str) and checks if the hash already exists in the acceptedJobs table
+    """
     return ds.search_hash(hash, 'acceptedJobs')
+    
 def is_in_notaccepted(hash):
+    """
+    is_in_notaccepted: takes in a hash (str) and checks if the hash already exists in the notacceptedJobs table
+    """
     return ds.search_hash(hash, 'notacceptedJobs')
 
 
---
+++ src/job_posting_class.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class Job_Posting:
+    "Job posting class for storing all job attributes"
+    hash: str
+    title: str
+    date_posted: str
+    link: str
+    company: str
+    location: str
+    level: str
+    description: str
