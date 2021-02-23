c52dd589936dbbaac75b8b0e6df7156637fa29c1
Anna Goncharova <annab.goncharova@gmail.com>
2021-02-23 12:48:32 -0800

    Changed object instantiation, added small comments

--- src/linkedinbot.py
+++ src/linkedinbot.py
@@ -11,17 +11,16 @@ import re
 from job_posting_class import Job_Posting
 start_time = time()
 from collect_data import store_data
+from data_store import to_csv
 
 def launch_driver(driver, url):
-    # url = "https://www.linkedin.com/jobs/search/?f_E=2%2C1&f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD"
-    # url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD&redirect=false&position=1&pageNum=0&f_TP=1%2C2'
-    # getVars = {'f_E' : '2', 'f_TPR' : 'r86400', 'geoId': '102095887', 'keywords' : job_title, 'location' : location, 'sortBy': 'DD'}
-    # url = ('https://www.linkedin.com/jobs/search/?' + urllib.parse.urlencode(getVars))
-    # print("url: ", url)
-    # options = Options()
-    # options.add_argument('--headless')    # without opening Chrome window
+    """ Launch a web driver that will go to url, scroll to end of page 
+    and return a web container with all job postings
 
-    # driver = webdriver.Chrome('src/chromedriver', options=options)
+    Arguments:
+    driver -- a web driver (Chrome)
+    url -- page that web driver launches and collects info from
+    """
     driver.get(url)
     if not driver:
         raise RuntimeError('Driver object is incorrect.')
@@ -36,8 +35,8 @@ def launch_driver(driver, url):
         else:
             return False
     last_height = driver.execute_script('return document.body.scrollHeight')
-    flag = 1
-    while flag == 1:
+    endOfPage = False
+    while endOfPage == False:
       driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
       try:
         wait = WebDriverWait(driver, 10)
@@ -45,9 +44,8 @@ def launch_driver(driver, url):
         last_height = new_height
       except:
           print("End of page reached")
-          flag = 0
+          endOfPage = True
 
-    # pageSource = driver.page_source
     lxml_soup = BeautifulSoup(driver.page_source, 'lxml')
 
     job_container = lxml_soup.find('ul', class_ = 'jobs-search__results-list')
@@ -56,63 +54,58 @@ def launch_driver(driver, url):
 
 
 def extract_data(driver, job_container):
-    ''' 
-    driver -> web driver, Chrome
-    job_container -> a web container with all job postings
-    Extracts all jobs postings and theri attributes from the job container
-    Returns a list of Job_Posting objects 
-    '''
+    """ Extract data from job_container and return a list of Job_Posting objects
+
+    Arguments:
+    driver -- a web driver (Chrome)
+    job_container -- an html element on the page that contains all job postings
+    """
     if len(job_container) == 0:
         raise ValueError('Job container is empty. To proceed, it must contain at least one item.')
     
-    job_postings = []
-
+    job_list = []
 
     for i in range(1, len(job_container)+1):
-        # print(i)
-        # job_id = i.find('a', href=True)['href']
-        # job_id = re.findall(r'(?!-)([0-9]*)(?=\?)',job_id)[0]
-        # # job_postings.append(job_id)
-        # print(f'job_id = {job_id}')
-
         try:
             job_xpath = f'/html/body/main/div/section/ul/li[{i}]/img'
             driver.find_element_by_xpath(job_xpath).click()
-            sleep(3)
-            job = Job_Posting
-
-            job.title = driver.find_element_by_class_name('topcard__title').text
-
-            job.link = driver.find_element_by_xpath('/html/body/main/section/div[2]/section/div[1]/div[1]/a').get_attribute('href')
-
-            job.company = driver.find_element_by_class_name('topcard__flavor').text
-
-            job.location = driver.find_element_by_class_name('topcard__flavor--bullet').text
-
-            job.level = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/ul/li[1]/span').text
-
-            job.description = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/div/section/div').text
-
-            job.date_posted = date.today().strftime("%m/%d/%y")
-
-            job_postings.append(job)
+            sleep(3)    # so that driver does not flood the page with requests
+            job = Job_Posting(
+                title=driver.find_element_by_class_name('topcard__title').text,
+                title = driver.find_element_by_class_name('topcard__title').text,
+                link = driver.find_element_by_xpath('/html/body/main/section/div[2]/section/div[1]/div[1]/a').get_attribute('href'),
+                company = driver.find_element_by_class_name('topcard__flavor').text,
+                location = driver.find_element_by_class_name('topcard__flavor--bullet').text,
+                level = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/ul/li[1]/span').text,
+                description = driver.find_element_by_xpath('/html/body/main/section/div[2]/section[2]/div/section/div').text,
+                date_posted = date.today().strftime("%m/%d/%y")
+            )
+            job_list.append(job)
 
         except Exception as e:
             print('Runtime error while processing a job posting')
             print(e.args)
             print(e.with_traceback)
 
-    return job_postings
+    return job_list
 
 
 if __name__ == "__main__":
+    # SEARCH_POSITION
+    # SEARCH_LOCATION
+    # HEADLESS = True
     url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=DD&redirect=false&position=1&pageNum=0&f_TP=1%2C2'
     options = Options()
     options.add_argument('--headless')
     driver = webdriver.Chrome('src/chromedriver', options=options)
 
     job_container = launch_driver(driver, url)
-    job_postings = extract_data(driver, job_container)
+    job_list = extract_data(driver, job_container)
     
-    store_data(job_postings)
+    store_data(job_list)
+    to_csv('acceptedJobs')
+    to_csv('notacceptedJobs')
+    driver.quit()       # closes all windows, release resources
+
+
 
