054944437c2422b59de064988163db81a1832df5
Anna Goncharova <annab.goncharova@gmail.com>
2021-03-02 16:20:39 -0800

    Removed unused functions

--- src/hash.py
+++ src/hash.py
@@ -2,27 +2,14 @@ import hashlib
 import data_store as ds
 
 def get_hash(job_description):
-    """
-    get_hash: takes in a text and returns a hash key
-    """
+    """ Takes in a text and returns a hash key """
+
     job_description = job_description.encode('utf-8')
     hash_object = hashlib.md5(job_description.lower())
     return hash_object.hexdigest()
 
-def is_in_accepted(hash):
-    """
-    is_in_accepted: takes in a hash (str) and checks if the hash already exists in the acceptedJobs table
-    """
-    return ds.search_hash(hash, 'acceptedJobs')
-    
-def is_in_notaccepted(hash):
-    """
-    is_in_notaccepted: takes in a hash (str) and checks if the hash already exists in the notacceptedJobs table
-    """
-    return ds.search_hash(hash, 'notacceptedJobs')
-
 def is_seen(hash):
-    """ Check if hash is already in the table """
+    """ Checks if hash is already in the table """
     
     return ds.search_hash(hash)
 
