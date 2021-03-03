dcd8a8a61bb297ddb3ca7bd03da940533b50be4a
Anna Goncharova <annab.goncharova@gmail.com>
2021-03-02 16:17:40 -0800

    Added argv support, url parsing, exception logging

--- src/linkedinbot.py
+++ src/linkedinbot.py
@@ -1,3 +1,4 @@
+from os import system
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -5,8 +6,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from time import time
 from time import sleep
-from datetime import date
-import traceback
+from datetime import datetime
+import sys
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+import logging
+logging.basicConfig(filename='extraction.log',level=logging.ERROR)
 
 from job_posting_class import Job_Posting
 start_time = time()
@@ -46,11 +51,12 @@ def launch_driver(driver, url):
           print("End of page reached")
           endOfPage = True
 
+    # TODO: check if there's a button "Show more" and click it
+
     lxml_soup = BeautifulSoup(driver.page_source, 'lxml')
 
     job_container = lxml_soup.find('ul', class_ = 'jobs-search__results-list')
     print('Job postings found:', len(job_container))
-    # print(job_container)
     return job_container
 
 
@@ -65,12 +71,13 @@ def extract_data(driver, job_container):
         raise ValueError('Job container is empty. To proceed, it must contain at least one item.')
     
     job_list = []
-
+    errors = 0
     for i in range(1, len(job_container)+1):
         try:
             job_xpath = f'/html/body/main/div/section[2]/ul/li[{i}]/img'
             driver.find_element_by_xpath(job_xpath).click()
-            sleep(3)    # so that driver does not flood the page with requests
+            
+            sleep(3)    # wait for all components to get loaded after clicking
             job = Job_Posting(
                 hash = '',
                 title = driver.find_element_by_class_name('topcard__title').text,
@@ -79,24 +86,36 @@ def extract_data(driver, job_container):
                 location = driver.find_element_by_class_name('topcard__flavor--bullet').text,
                 level = driver.find_element_by_class_name('job-criteria__text--criteria').text,
                 description = driver.find_element_by_class_name('description__text--rich').text,
-                date_posted = date.today().strftime("%m/%d/%y"),
+                date_posted = datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
                 accepted = False
             )
 
             job_list.append(job)
 
         except Exception:
-            print(f'Runtime error while processing a job posting')
-            print(traceback.format_exc())
-
+            errors += 1
+            logging.exception('Runtime error while processing a job posting')
+    print(f'Errors encountered: {errors}')
     return job_list
+
+def create_url(url, params):
+    """ Create a full url from the short url and search parameters
+
+    """
+    param_dict = {'keywords': params[0], 'location': params[1], 'sortBy': 'R'}
+    full_url = url + urlencode(param_dict)
+    return full_url
 
 
 if __name__ == "__main__":
-    # SEARCH_POSITION
-    # SEARCH_LOCATION
-    HEADLESS = True    # TODO: add this as kwargs
-    url = 'https://www.linkedin.com/jobs/search/?f_TP=1%2C2&f_TPR=r86400&geoId=102095887&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=R'
+    print(len(sys.argv))
+    SEARCH_POSITION = sys.argv[1]
+    SEARCH_LOCATION = sys.argv[2]
+    HEADLESS = True         # do not open Chrome 
+    url = create_url('https://www.linkedin.com/jobs/search/?', [SEARCH_POSITION, SEARCH_LOCATION])
+    # url = 'https://www.linkedin.com/jobs/search/?f_TP=1%2C2&f_TPR=r86400&keywords=software%20engineer&location=California%2C%20United%20States&sortBy=R'
+
+
     options = Options()
     if HEADLESS:
         options.add_argument('--headless')
@@ -107,8 +126,9 @@ if __name__ == "__main__":
     
     store_data(job_list)
     to_csv(True)        # output accepted jobs to csv file
-    to_csv(False)       # output not accepted jobs into csv file
-    driver.quit()       # closes all windows, release resources
+    # to_csv(False)       # output not accepted jobs into csv file, uncomment if needed
+    
+    driver.quit()       # close all windows, release resources
 
 
 
